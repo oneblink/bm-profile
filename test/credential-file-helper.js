27289959f49a15631e602493c2daa2a0ce3e4f29
simonmarklar <simondobie23@gmail.com>
2016-05-17 12:04:58 +1000

    added credentials -> json parsing

---
+++ test/credential-file-helper.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const path = require('path');
+
+const test = require('ava');
+const mockery = require('mockery');
+
+const credentialFileHelper = '../lib/credential-file-helper.js';
+
+const convertPath = (p) => p.replace(/\//g, path.sep);
+
+test.beforeEach(() => {
+  mockery.enable({ useCleanCache: true });
+  mockery.registerAllowable(credentialFileHelper, true);
+  mockery.registerAllowables(['os', 'path', 'readline']);
+});
+
+test.afterEach(() => {
+  mockery.warnOnUnregistered(true);
+  mockery.deregisterAll();
+  mockery.resetCache();
+  mockery.disable();
+});
+
+test.serial('it should set the path values to default if they are not supplied', (t) => {
+  mockery.registerAllowables(['stream', 'util', './credential-parser.js', 'fs']);
+  const CredentialHelper = require(credentialFileHelper);
+  const c = new CredentialHelper();
+
+  t.same(c.path, CredentialHelper.DEFAULT_PATH);
+});
+
+test.serial('non-existant paths should be invalid', (t) => {
+  mockery.registerAllowables(['stream', 'util', './credential-parser.js']);
+  const fsModule = {
+    access: (p, cb) => cb(new Error('does not exist'))
+  };
+
+  mockery.registerMock('fs', fsModule);
+  const CredentialHelper = require(credentialFileHelper);
+  const c = new CredentialHelper(convertPath('/i/dont/exist'));
+
+  t.throws(c.isValidFilePath());
+});
+
+test.serial('existing paths should be valid', (t) => {
+  mockery.registerAllowables(['stream', 'util', './credential-parser.js']);
+  const fsModule = {
+    access: (p, cb) => cb()
+  };
+
+  mockery.registerMock('fs', fsModule);
+  const CredentialHelper = require(credentialFileHelper);
+  const c = new CredentialHelper();
+
+  return c.isValidFilePath();
+});
+
+test.serial('it should find the default profile', (t) => {
+  mockery.registerAllowables(['fs', 'stream', 'util', './credential-parser.js']);
+  const CredentialHelper = require(credentialFileHelper);
+  const c = new CredentialHelper('./fixtures/default-only');
+
+  return c.profileExists('default').then((found) => t.same(found, 'default'));
+});
+
+test.serial('it should reject if the profile doesnt exist', (t) => {
+  mockery.registerAllowables(['fs', 'stream', 'util', './credential-parser.js']);
+  const CredentialHelper = require(credentialFileHelper);
+  const c = new CredentialHelper('./fixtures/default-only');
+
+  t.throws(c.profileExists('doesnt_exist'));
+});
---
+++ test/credential-parser.js
@@ -0,0 +1,124 @@
+'use strict';
+
+const fs = require('fs');
+
+const test = require('ava');
+const mockery = require('mockery');
+const streamTransformer = '../lib/credential-parser.js';
+
+test.cb.beforeEach((t) => {
+  mockery.enable({ useCleanCache: true });
+  mockery.registerAllowable(streamTransformer, true);
+  mockery.registerAllowables(['stream', 'util']);
+  t.end();
+});
+
+test.cb.afterEach((t) => {
+  mockery.warnOnUnregistered(true);
+  mockery.deregisterAll();
+  mockery.resetCache();
+  mockery.disable();
+  t.end();
+});
+
+test.cb('it turns a single profile into an array with 3 entries', (t) => {
+  const fixture = './fixtures/default-only';
+  const transformer = require(streamTransformer)();
+  fs.readFile(fixture, {encoding: 'utf-8'}, (err, data) => {
+    if (err) {
+      return t.end(err);
+    }
+    const result = transformer.chunkToArray(data);
+    t.is(result.length, 3);
+    t.is(result[0], '[default]');
+    t.is(result[1], 'aws_access_key_id = 11111111111111111111');
+    t.is(result[2], 'aws_secret_access_key = 2222222222222222222222222222222222222222');
+    t.end();
+  });
+});
+
+test.cb('it reads three profiles', (t) => {
+  const fixture = './fixtures/three';
+  const transformer = require(streamTransformer)();
+  const credentialsFile = fs.createReadStream(fixture);
+  const expectedProfileNames = ['default', 'first', 'second'];
+  const expectedNumReads = 3;
+  t.plan(expectedNumReads);
+  let i = 0;
+  const finish = () => ++i === expectedNumReads ? t.end() : null;
+
+  transformer.on('readable', () => {
+    const data = transformer.read();
+    if (!data) {
+      return;
+    }
+
+    const profileName = Object.keys(data)[0];
+    t.true(expectedProfileNames.indexOf(profileName) > -1);
+    finish();
+  });
+
+  credentialsFile.pipe(transformer);
+});
+
+test.cb('it reads three profiles even when a profile is incomplete', (t) => {
+  const fixture = './fixtures/malformed';
+  const transformer = require(streamTransformer)();
+  const credentialsFile = fs.createReadStream(fixture);
+  const expectedProfileNames = ['default', 'first', 'second'];
+  const expectedNumReads = 5;
+  t.plan(expectedNumReads);
+  let i = 0;
+  const finish = () => ++i >= 3 ? t.end() : null;
+
+  transformer.on('readable', () => {
+    const data = transformer.read();
+    if (!data) {
+      return;
+    }
+
+    const profileName = Object.keys(data)[0];
+    t.true(expectedProfileNames.indexOf(profileName) > -1);
+    if (profileName === expectedProfileNames[1]) {
+      t.true(!!data[profileName].aws_access_key_id);
+      t.false(!!data[profileName].aws_secret_access_key);
+    }
+    finish();
+  });
+
+  credentialsFile.pipe(transformer);
+});
+
+test.cb('it replaces the first profile', (t) => {
+  const fixture = './fixtures/malformed';
+  const firstProfile = {
+    first: {
+      aws_access_key_id: 'aaaaaaaaaaaaaaaaaaaa',
+      aws_secret_access_key: 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+    }
+  };
+  const transformer = require(streamTransformer)(firstProfile);
+  const credentialsFile = fs.createReadStream(fixture);
+  const expectedProfileNames = ['default', 'first', 'second'];
+  const expectedNumReads = 5;
+  t.plan(expectedNumReads);
+  let i = 0;
+  const finish = () => ++i >= 3 ? t.end() : null;
+
+  transformer.on('readable', () => {
+    const data = transformer.read();
+    if (!data) {
+      return;
+    }
+
+    const profileName = Object.keys(data)[0];
+    t.true(expectedProfileNames.indexOf(profileName) > -1);
+    if (profileName === 'first') {
+      t.same(data[profileName].aws_access_key_id, firstProfile.first.aws_access_key_id);
+      t.same(data[profileName].aws_secret_access_key, firstProfile.first.aws_secret_access_key);
+    }
+    finish();
+  });
+
+  credentialsFile.pipe(transformer);
+});
