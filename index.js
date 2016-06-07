164dc7d8af25e74b28802478df0d27825cef0e0c
simonmarklar <simondobie23@gmail.com>
2016-06-07 11:02:55 +1000

    Update warning about node TTY issue to not be platform specific.

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ module.exports = function (input, flags) {
   if (isBroken()) {
     console.log(`
 ############################################################################
-The version of node that you are running has issues with prompts on windows. 
-This issue does not negatively effect our CLI, however you are encouraged to
+The version of node that you are running has issues with TTY input. 
+These issues do not negatively effect our CLI, however you are encouraged to
 update your version of node.
 
 Please upgrade to nodejs v6.2.0 or higher, or downgrade to lower than v5.6.0
